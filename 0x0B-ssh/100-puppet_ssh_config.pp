ec5f16ff4d2c9f0710da0043efd816bcb9bd0b8b
Cebo59 <cebokuhledhlamini59@gmail.com>
2023-04-25 11:47:32 -0700

    Files

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -8,7 +8,7 @@
 
 #file_line { 'Turn off passwd auth':
 #  path    => '/etc/ssh/ssh_config',
-#  line    => '    PasswordAuthentication yes',
+#  line    => '    PasswordAuthentication no',
 #  replace => true,
 #}
 
@@ -17,7 +17,7 @@
 $file_content = file('/etc/ssh/ssh_config')
 $config = "${file_content}\
     IdentityFile ~/.ssh/school
-    PasswordAuthentication yes
+    PasswordAuthentication no
 "
 file {  'school':
   ensure  => 'present',
